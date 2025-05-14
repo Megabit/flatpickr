9302c78ed698f1cb146f0857c41033c5fb23cb4f
Mladen Macanovic <mladen.macanovic@blazorise.com>
2025-05-14 11:27:44 +0200

    Fix isMobile detection

--- src/index.ts
+++ src/index.ts
@@ -2181,9 +2181,11 @@ function FlatpickrInstance(
       !self.config.disable.length &&
       !self.config.enable &&
       !self.config.weekNumbers &&
-      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Opera Mobi|Mobile|Silk|Kindle|Windows Phone|PlayBook|BB10|MeeGo|Tizen|Puffin/i.test(
         navigator.userAgent
-      );
+      ) ||
+        "ontouchstart" in window ||
+        navigator.maxTouchPoints > 0);
 
     for (let i = 0; i < self.config.plugins.length; i++) {
       const pluginConf = self.config.plugins[i](self) || ({} as Options);
